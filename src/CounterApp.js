87d540a40ab88c97dcca46b5719910e55f890278
ios <iam.ivansantana@gmail.com>
2021-05-20 14:34:27 -0500

    last commit

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -24,9 +24,9 @@ const CounterApp = ({value})=>{
         return(
             <>
                 <h1>CounterApp</h1>
-                <h2 >{counter}</h2>
                 <button onClick={handleAdd}>+1</button>
-                <button onClick={handleReset}>Reset</button>
+                {/* <button onClick={handleReset}>Reset</button> */}
+                <h2 >{counter}</h2>
                 <button onClick={handleSubtrack}>-1</button>
             </>
         );
